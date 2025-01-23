5ad42f4bf8067350097932e47190b960c4867522
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-01-23 22:09:38 +0100

    Update

--- Heart Attacks in Russia/Heart Attacks Analysis.py
+++ Heart Attacks in Russia/Heart Attacks Analysis.py
@@ -12,4 +12,18 @@ print(Hrt_A.info())
 
 print(Hrt_A.describe())
 
-print(Hrt_A['Age'].max())
+print(f"Highest Age recorded is {Hrt_A['Age'].max()}")
+
+#check relations between obesity and heart attacks 
+Obe_Att_Tru = Hrt_A['Obesity'] == True , Hrt_A['Heart_Attack']==True
+Obe_Att_Fal = Hrt_A['Obesity'] == False , Hrt_A['Heart_Attack']==True
+print(Obe_Att_Fal)
+print(Obe_Att_Tru)
+print(len(Obe_Att_Tru))
+print(len(Obe_Att_Fal))
+
+#check relations between stress level and heart attacks 
+
+#check relations urban/rural level and heart attacks 
+
+#check relations between daily water intake and 
