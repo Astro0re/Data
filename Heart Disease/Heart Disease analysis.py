65b8a1dde3caabc07a3a03059ce0683937540784
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-01-27 13:18:57 +0100

    Update

--- Heart Disease/Heart Disease analysis.py
+++ Heart Disease/Heart Disease analysis.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import numpy as nu
-import matplotlib as mat
 
 Hrt =pd.read_csv(r"C:\Users\USER\Downloads\Dataset Heart Disease.csv")
 
-print(Hrt.head())
+view_info = Hrt.head()
 
 print(Hrt.shape)
 
-print(Hrt.info())
+Hrt.info()
 
-print(Hrt.describe())
+view_data_details = Hrt.describe()
