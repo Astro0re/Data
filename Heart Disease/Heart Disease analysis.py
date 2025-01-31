2eeab4f45c44546048132adde7482b7e0b80b1f8
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-01-31 11:20:40 +0100

    Update

--- Heart Disease/Heart Disease analysis.py
+++ Heart Disease/Heart Disease analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import numpy as nu 
+import numpy as nu
+import matplotlib as mt
+
 Hrt =pd.read_csv(r"C:\Users\USER\Downloads\Dataset Heart Disease.csv")
 
 view_info = Hrt.head()
@@ -13,3 +15,9 @@ Hrt.info()
 view_data_details = Hrt.describe()
 
 print(Hrt['age'].value_counts())
+
+nu.abs(.45)
+
+
+Density_Plot =Hrt['age'].plot(kind='box' , vert=True, figsize=(14,6))
+
