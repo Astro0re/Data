308fdaa369c93973291e9445ee6ddde1bea0cb28
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-02-16 18:53:33 +0100

    Update

--- Monkey _Pox/M_Pox Analysis.R
+++ Monkey _Pox/M_Pox Analysis.R
@@ -174,7 +174,7 @@ Afri %>%
   facet_wrap(~location)
 
 Afri %>%
-  filter(new_cases > 10)%>% 
+  filter(new_cases > mean(new_cases))%>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -196,7 +196,7 @@ Euro %>%
   facet_wrap(~location)
 
 Euro %>% 
-  filter(new_cases > 10)%>% 
+  filter(new_cases > mean(new_cases))%>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -219,7 +219,7 @@ Asia %>%
   facet_wrap(~location)
 
 Asia %>% 
-  filter(new_cases > 10)%>% 
+  filter(new_cases > mean(new_cases))%>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -242,7 +242,7 @@ Isl %>%
   facet_wrap(~location)
 
 Isl %>% 
-  filter(new_cases > 10)%>% 
+  filter(new_cases > mean(new_cases))%>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -265,7 +265,7 @@ Ocea %>%
   facet_wrap(~location)
 
 Ocea %>% 
-  filter(new_cases > 10) %>% 
+  filter(new_cases > mean(new_cases)) %>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -288,7 +288,7 @@ Nt_Ame %>%
   facet_wrap(~location)
 
 Nt_Ame %>% 
-  filter(new_cases > 10) %>% 
+  filter(new_cases > mean(new_cases)) %>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -310,7 +310,7 @@ St_Ame %>%
   facet_wrap(~location)
 
 St_Ame %>% 
-  filter(new_cases > 10) %>% 
+  filter(new_cases > mean(new_cases)) %>% 
   ggplot(aes(Date, new_cases))+
   geom_point()+
   facet_wrap(~location)
@@ -330,10 +330,9 @@ Un_Spe %>%
     geom_point()
 
 Un_Spe %>%
-  filter(new_cases > 10) %>% 
+  filter(new_cases > mean(new_cases)) %>% 
   ggplot(aes(Date, new_cases))+
-  geom_point()+
-  facet_wrap(~location)
+  geom_point()
 
 Un_Spe %>%
   ggplot(aes(total_cases, total_deaths))+
