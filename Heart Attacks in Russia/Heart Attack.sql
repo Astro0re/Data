fdf0eaeb090019f9202a39ad2aae6e6daced9ba1
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-03-16 21:59:10 +0100

    Sql Querys

---
+++ Heart Attacks in Russia/Heart Attack.sql
@@ -0,0 +1,9 @@
+-- @block
+select * from`heart_attack_russia_data 2`
+where Diet = 'Unhealthy'
+group by Income_Level
+
+-- @block
+select * from`heart_attack_russia_data 2`
+where Diet = 'Unhealthy'
+group by Income_Level
