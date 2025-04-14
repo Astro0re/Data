033a07d1ee6a0444d5766245b4d7ffbfc6d89ca6
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-14 12:00:59 +0100

    SQL Update

--- Heart Attacks in Russia/Heart Attack Query.sql
+++ Heart Attacks in Russia/Heart Attack Query.sql
@@ -17,3 +17,29 @@ order by Age;
 select Gender, COUNT(ID)
 from `heart_attack_russia_data 1`
 group by Gender;
+
+# Unserious data lol 
+select *
+from `heart_attack_russia_data 1`
+where Gender = "Other";
+
+# Condition 
+# High Stress level and Heart rate
+select Heart_Attack , count(ID)
+from `heart_attack_russia_data 1`
+where Stress_Level > 5 and Heart_Rate > 70
+group by Heart_Attack;
+
+# Low Exercise more likely to lead to heart attack ?
+# Not really significant
+select Exercise_Level , count(ID)
+from `heart_attack_russia_data 1`
+where Heart_Attack = 'TRUE'
+group by Exercise_Level;
+
+# Diet in relation to heart disease in general
+# No significance noticed
+select Diet , count(ID)
+from `heart_attack_russia_data 2`
+where Heart_Disease_History = 'TRUE'
+group by Diet;
