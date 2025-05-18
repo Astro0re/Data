f9eea2d010a4fa827de91348c545e4624335a425
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-18 16:45:17 +0100

    Code Update

--- Lung Cancer/lung_cancer analysis.R
+++ Lung Cancer/lung_cancer analysis.R
@@ -2,4 +2,8 @@ library(tidyverse)
 library(Hmisc)
 library(ggplot2)
 
-lung <- read.csv("C:/Users/USER/Documents/Git_Hub Data/Data/Lung Cancer/Lung_Cancer_Trends_Realistic.csv")
+lung_c <- read.csv("C:/Users/USER/Documents/Git_Hub Data/Data/Lung Cancer/Lung_Cancer_Trends_Realistic.csv")
+
+describe(lung)
+
+head(lung_c)
