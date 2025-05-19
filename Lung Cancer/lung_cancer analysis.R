0fd6fe15e4ec43ac14107b0f88f6e4c194922c42
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-19 18:57:16 +0100

    Data Explorations

--- Lung Cancer/lung_cancer analysis.R
+++ Lung Cancer/lung_cancer analysis.R
@@ -7,3 +7,23 @@ lung_c <- read.csv("C:/Users/USER/Documents/Git_Hub Data/Data/Lung Cancer/Lung_C
 describe(lung)
 
 head(lung_c)
+
+# Smocking_Status
+table(lung_c$Smoking_Status)
+
+# Works but does not display properly
+# Output wrong 
+Smoke_stat <- lung_c %>% 
+  group_by(Smoking_Status) %>% 
+  summarise(Lung_Cancer = table(lung_c$Lung_Cancer_Stage))
+print(Smoke_stat)
+  
+
+# Secondhand_Smoke
+table(lung_c$Secondhand_Smoke_Exposure)
+
+Smoke_stat <- lung_c %>% 
+  group_by(Secondhand_Smoke_Exposure) %>% 
+  summarise(Lung_Cancer = table(lung_c$Lung_Cancer_Stage))
+print(Smoke_stat)
+
