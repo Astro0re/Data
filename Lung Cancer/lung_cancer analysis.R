b8b8418476886f36eb42e4322224001b094e3c5e
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-26 19:19:43 +0100

    Data Viz Update and code clean up

--- Lung Cancer/lung_cancer analysis.R
+++ Lung Cancer/lung_cancer analysis.R
@@ -13,39 +13,36 @@ table(lung_c$Smoking_Status)
 
 
 # Data Viz
-#Gender 
-lung_c %>% ggplot(aes(Gender,0.1))+
+#Gender
+lung_c %>% ggplot(aes(Gender, 0.1)) +
   geom_col()
 #Run T/AB testing on gender
-lung_c %>% ggplot(aes(Gender, 1))+
-  geom_col()+
+lung_c %>% ggplot(aes(Gender, 1)) +
+  geom_col() +
   facet_wrap(~lung_c$Lung_Cancer_Stage)
 
-# Smoking status  
+# Smoking status
 # Smoke stats never should relate(display 0) to years_smoking and cigarettes_per_day ??
-lung_c %>% ggplot(aes(Smoking_Status, 1))+
+lung_c %>% ggplot(aes(Smoking_Status, 1)) +
   geom_col()
 
 # Secondhand_Smoke
 table(lung_c$Secondhand_Smoke_Exposure)
 
-lung_c %>% ggplot(aes(Secondhand_Smoke_Exposure, 1))+
-  geom_col()+
+lung_c %>% ggplot(aes(Secondhand_Smoke_Exposure, 1)) +
+  geom_col() +
   facet_wrap(~lung_c$Lung_Cancer_Stage)
 #This Graph shows that second hand smoke exposure does not directly relate to lung cancer
 
-#Occupational exposure 
-lung_c %>% filter(Lung_Cancer_Stage != 'None') %>% 
-  ggplot(aes(Occupation_Exposure, 1))+
-  geom_col()+
+#Occupational exposure
+lung_c %>% filter(Lung_Cancer_Stage != "None") %>%
+  ggplot(aes(Occupation_Exposure, 1)) +
+  geom_col() +
   facet_wrap(~lung_c$Lung_Cancer_Stage)
 
 table(lung_c$Occupation_Exposure)
 
-#Family History 
-lung_c %>% ggplot(aes(Family_History, 1))+
-  geom_col()+
+#Family History
+lung_c %>% ggplot(aes(Family_History, 1)) +
+  geom_col() +
   facet_wrap(~lung_c$Lung_Cancer_Stage)
-
-
-
