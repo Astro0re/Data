ad41ac2a3a5ef31e52d4951e84aa52e08f0f265c
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-06-19 05:48:20 +0100

    Remove File

--- Glass/Glass Analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib as mat
-
-glass=pd.read_csv(r"C:\Users\USER\Documents\Git_Hub Data\Data\Glass\glass.csv")
-
-print(glass.head())
-print(glass.info())
-print(glass.describe())
