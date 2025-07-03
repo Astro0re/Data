db357dc46fccf8ed27fb7d15b1022c9edde64168
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-07-03 05:32:33 +0100

    Code Update

--- Heart Attacks in Russia/Heart Attack Query.sql
+++ Heart Attacks in Russia/Heart Attack Query.sql
@@ -198,3 +198,11 @@ join `heart_attack_russia_data 2` as d2
 	on d1.ID = d2.ID 
 group by Income_Level
 ;
+
+select Obesity, avg(Blood_Pressure),avg(Heart_Rate), count(Family_History = 'TRUE'), avg(Health_Awareness),
+ avg(Daily_Water_Intake),avg(Daily_Water_Intake), count(Heart_Attack = 'TRUE'),  count(Heart_Attack = 'FALSE')
+from `heart_attack_russia_data 1` as d1
+join `heart_attack_russia_data 2` as d2
+	on d1.ID = d2.ID 
+group by Obesity
+;
